b7d76d1b61b1152a27a8439873504bf1d700ab9c
Mezrha <149977691+Mezrha@users.noreply.github.com>
2023-11-13 16:07:32 +0100

    Version finale

---
+++ eda/barplot1.py
@@ -0,0 +1,75 @@
+import panel as pn
+import pandas as pd
+from dataloadre import df
+import hvplot.pandas
+df = pd.read_csv('dataset.csv')
+pn.extension(sizing_mode='stretch_width')
+plot_opts = dict(
+    responsive=True, min_height=400,
+    # Align the curves' color with the template's color
+    color=pn.template.FastListTemplate.accent_base_color
+)
+
+class BarPlotter:
+    def __init__(self, data):
+        self.data = data
+        self.filtered_data = data.copy()
+
+        # Create filter options
+        filter_options = list(self.data[["album_name", "track_name", "track_genre", 'artists']])
+        self.filter_selector = pn.widgets.Select(name='Select Filter', options=filter_options, value=filter_options[3])
+
+        # Initialize an empty plot using HoloViews
+        self.bar_plot_pane = pn.pane.HoloViews()
+
+        # Combine the Plot and Selector using Panel
+        self.layout = pn.Column(
+            pn.Row(self.filter_selector),
+            pn.Row(self.bar_plot_pane, sizing_mode='stretch_width')
+        )
+
+        # Register callback for widget changes
+        self.filter_selector.param.watch(self._update_plot, 'value')
+
+        # Initial plot
+        self._update_plot()
+
+    def _create_plot(self):
+        filter_value = self.filter_selector.value
+
+        if filter_value not in self.data.columns:
+            return None  # Handle the case where the selected column doesn't exist
+
+        # Get the top 10 values for the selected column based on popularity
+        top_values = self.data.groupby(filter_value)['popularity'].mean().nlargest(15).index.tolist()
+
+        # Filter the data for the top 10 values
+        filtered_data_top10 = self.data[self.data[filter_value].isin(top_values)]
+
+        # Use hvplot to create the Barplot
+        bar_plot = filtered_data_top10.hvplot.bar(
+            x=filter_value,
+            y="Popularity",
+            xlabel="",
+            ylabel="Popularity",
+            title=f'Top 10 Popularity by {filter_value}',
+            rot=45,
+            height=700,
+            #width=1250,
+            **plot_opts,
+            sizing_mode='stretch_width'
+        )
+
+        return bar_plot
+
+    def _update_plot(self, event=None):
+        # Triggered when the filter_selector value changes
+        bar_plot = self._create_plot()
+
+        # Update the Pane with the new plot
+        self.bar_plot_pane.object = bar_plot
+
+bar_plotter = BarPlotter(df)  # Replace "your_data" with your actual dataset
+
+# Make the BarPlotter instance servable
+bar_plotter.layout.servable()
